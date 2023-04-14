549be42a16be8a6876887fbb00d9a4ea71f8bb13
Vilem-McKael <vilemckael@gmail.com>
2023-04-13 23:30:03 -0700

    implements basic audio playback according to a preset rhythm

---
+++ src/components/Track/Track.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import kick1 from '../../../src/assets/sounds/kick1.wav'
+import useSound from 'use-sound';
+
+export default function Track({}) {
+
+    const [trackInputs, setTrackInputs] = useState([1, 1, 1, 0, 1, 0, 1, 1]);
+    const [audio, setAudio] = useState(new Audio(kick1));
+    const [playSound, {stop}] = useSound(kick1, {volume: .25, interrupt: true});
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(function() {
+        // https://upmostly.com/tutorials/build-a-react-timer-component-using-hooks
+        let playback = null;
+        if (isPlaying) {
+            playback = setInterval(() => {
+                if (isPlaying && trackInputs[trackIndex]) {
+                    playSound();
+                } else {
+                    stop();
+                }
+                setTrackIndex((trackIndex + 1) % 8);
+                console.log(trackIndex);
+            }, 300);
+        } else {
+            clearInterval(playback);
+            setTrackIndex(0);
+        }
+
+        return () => clearInterval(playback);
+    }, [isPlaying, trackInputs, trackIndex]);
+
+    function stopPlayback() {
+        stop();
+        setIsPlaying(false);
+    }
+
+    function startPlayback() {
+        setIsPlaying(true);
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={playSound}>Kick</button>
+                <button onClick={stopPlayback}>Stop</button>
+                <button onClick={startPlayback}>Start</button>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Project/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Track from '../../components/Track/Track'
+
+export default function Project() {
+  return (
+    <div>
+      <h1>Project</h1>
+      <Track />
+    </div>
+  )
+}
+
